cc86a35551199a9b2342537c67ab99298665f8db
smu <smu@z107.de>
2022-12-07 17:20:22 +0000

    day 07

--- day07/day07.py
+++ day07/day07.py
@@ -32,6 +32,7 @@ class Dir:
         return([c.name for c in self.childs])
 
     def get_child(self, name):
+        # print('Wechsle in %s ' % name)
         for c in self.childs:
             if c.name == name:
                 return(c)
@@ -44,9 +45,9 @@ class Dir:
         if name not in self.get_child_names():
             child = Dir(name, self)
             self.childs.append(child)
-            print("Child %s created" % name)
-        else:
-            print("Child %s exists" % name)
+            # print("Child %s created" % name)
+        # else:
+            # print("Child %s exists" % name)
 
     def calc_size(self):
         self.size = 0
@@ -56,7 +57,16 @@ class Dir:
         for f in self.files:
             self.size += f.size
 
+    def print_directory_structure(self,):
+        print("%s [%d]" % (self.full_path(), self.size))
+        for f in self.files:
+            print("F: %s/%s [%d]" % (self.full_path(), f.name, f.size))
+        for c in self.childs:
+            c.print_directory_structure()
+
     def get_total_size(self, limit):
+        ''' the result for part A '''
+
         total_size = 0
         if self.size <= limit:
             print("++ %s [%d]" % (self.full_path(), self.size))
@@ -64,20 +74,18 @@ class Dir:
         else:
             print("-- %s [%d]" % (self.full_path(), self.size))
         for c in self.childs:
-            subdir_size = c.get_total_size(limit)
-            # subdir_size = c.size
-            if subdir_size == 0:
-                continue
-            if subdir_size <= limit:
-                total_size += subdir_size
+            total_size += c.get_total_size(limit)
         return(total_size)
 
-    def print_directory_structure(self,):
-        print("%s [%d]" % (self.full_path(), self.size))
-        for f in self.files:
-            print("F: %s/%s [%d]" % (self.full_path(), f.name, f.size))
+    def answer_to_part_b(self, minsize):
+        # create a list of directories > minsize
+        dirlist = []
+        if self.size >= minsize:
+            dirlist.append(self)
         for c in self.childs:
-            c.print_directory_structure()
+            dirlist.extend(c.answer_to_part_b(minsize))
+        return(dirlist)
+
         
 
 cmd_re = re.compile("^\$")
@@ -89,25 +97,31 @@ root = Dir("/", None)
 current_dir = None
 
 with open("input") as f:
+# with open("input_example") as f:
     for line in f.readlines():
         line = line.strip()
         if re.match(cmd_re, line):
-            print("cmd: %s" % line)
+            # print("cmd: %s" % line)
             if re.match(cd_cmd_re, line):
                 target_dir = cd_cmd_re.findall(line)[0]
                 if target_dir == "/":
                     current_dir = root
                 elif not re.match("/", target_dir):
                     if target_dir == "..":
+                        # print('Wechsle eine Ebene hoch')
                         current_dir = current_dir.parent
                     else:
                         # not / in path. target_dir is direct subdirectory
                         current_dir = current_dir.get_child(target_dir)
                 else:
-                    print(line)
+                    print("UNKOWN CMD: %s " % line)
                     sys.exit()
+            else:
+                # print('Command ignorered: %s' % line)
+                # should be ls
+                pass
         else:
-            print("---: %s" % line)
+            # print("---: %s" % line)
             if re.match(dir_info_re, line):
                 child_dir_name = dir_info_re.findall(line)[0]
                 #print("child dir: %s" % child_dir_name)
@@ -120,10 +134,34 @@ with open("input") as f:
         
 root.calc_size()
 # root.print_directory_structure()
+
     
 print("----------------------------------")
-print()
+print('Answer to part A')
 print(root.get_total_size(100000))
 
-# first guess 144228, wrong
-# first guess 144230, wrong
+
+
+print("----------------------------------")
+print('Answer to part B')
+
+disk_space = 70000000
+min_unused_space = 30000000
+
+free_space = disk_space - root.size
+print('Current free disk space: %d' % (free_space))
+space_to_free = min_unused_space - free_space
+print('We still need to delete: %d' % (space_to_free))
+
+# choose the smallest directory from the list of directories with size > space_to_free
+dir_list = root.answer_to_part_b(space_to_free)
+minsize = 999999999999999999999
+for d in dir_list:
+    print("%s - %d" % (d.full_path(), d.size))
+    if d.size < minsize:
+        minsize = d.size
+        selected_directory = d
+
+
+print('lets delete:')
+print("%s - %d" % (selected_directory.full_path(), selected_directory.size))
